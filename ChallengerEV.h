a6aa0685b4c6fba80ea96239ff8548d251f0e501
BrettRD <brett.downing1@gmail.com>
2015-11-23 21:36:24 +0800

    added CAN IDs for telemetry

--- ChallengerEV.h
+++ ChallengerEV.h
@@ -14,20 +14,21 @@ const uint8_t brakeWarnBit = 4;
 const uint8_t throttleWarnBit = 5;
 
 
-
 struct WheelID{
-  const uint32_t managerID;   //ID of the wheel manager
+  const uint32_t managerID;   //ID of the wheel manager board
   const uint32_t motorReqID;  //ID of requests to the motor controller
   const uint32_t motorResID;  //ID of responses from the motor controller
+  const uint32_t dashID;      //ID the dashboard can recieve on.
+  const bool isFront;
 };  
 
 
 const int nWheels = 4;
 const WheelID wheel[nWheels] = {
-  {30,107,115},
-  {31,108,116},
-  {32,109,117},
-  {33,110,118}
+  {30,107,115,140, true},
+  {31,108,116,141, true},
+  {32,109,117,142, false},
+  {33,110,118,143, false}
 };
 
 
